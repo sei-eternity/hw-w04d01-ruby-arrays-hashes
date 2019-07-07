3e6e0d8a59c91d2edca572c5baf66c74f54bf739
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2019-07-07 21:37:54 +0300

    finished

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,51 @@
+def stops_between_stations()
 
-# your solution here
-def stops_between_stations
+    p 'enter starting line: '
+    start_line = gets.chomp
 
-end
+    p 'enter starting station'
+    start_station = gets.chomp
+
+    p 'enter ending line: '
+    end_line = gets.chomp
+
+    p 'enter ending station'
+    end_station = gets.chomp
+
+    lines = {
+    
+    'red': ['south station','park street','kendall','central','harvard', 'porter', 'davis' ,'alewife'],
+    
+    'green': ['government center','park street','boylston','arlington','copley','hynes', 'kenmore'],
+    
+    'orange': ['north station','haymarket','park street','state','cowntown crossing','chinatown','back bay','forest hills']
+    
+    }
+    
+    start_line.downcase!
+    start_station.downcase!
+    end_line.downcase!
+    end_station.downcase!
+    
+    start_line = start_line.to_sym
+    end_line = end_line.to_sym
+    
+    stops = 0
+    
+    if start_line == end_line 
+    
+    stops = (lines[start_line].index(start_station) - lines[start_line].index(end_station)).abs
+    
+    
+    else
+    
+    
+    stops += (lines[start_line].index(start_station) - lines[start_line].index('park street')).abs
+    
+    stops += (lines[end_line].index('park street') - lines[end_line].index(end_station)).abs
+    end
+    
+    stops
+    end
+    
+    
