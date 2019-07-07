cdea820b364ea1eb290f182aeb47a1dddcdb3ed3
ahlamcpp <a7lam.al.sa6am@hotmail.com>
2019-07-07 22:38:57 +0300

    HW W4D1-Ruby

--- mbta.rb
+++ mbta.rb
@@ -1,8 +1,7 @@
+red_line = ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"]
+green_line = ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"]
+orange_line = ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
 def stops_between_stations (start_line, start_station, end_line, end_station)
-Red_line = ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"]
-Green_line = ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"]
-Orange_line = ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
-
     total_stop=0
     start_stop = start_line.index(start_station)
     end_stop = end_line.index(end_station)
@@ -21,6 +20,6 @@ else
     end
 end
 
-# stops_between_stations(Red_line,"South Station",Green_line,"Kenmore") // 6
-# stops_between_stations(Red_line,"Alewife",Red_line,"South Station") //7
-# stops_between_stations(Red_line,"Alewife",Red_line,"Alewife") // 0
+# stops_between_stations(red_line,"South Station",green_line,"Kenmore") // 6
+# stops_between_stations(red_line,"Alewife",red_line,"South Station") //7
+# stops_between_stations(red_line,"Alewife",red_line,"Alewife") // 0
