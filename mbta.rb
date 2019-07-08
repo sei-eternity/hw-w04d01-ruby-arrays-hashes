84259b66aa55fd9c5fb31cb06fa783d912180f7f
Munira alshuhail <muniraalshuhail@Muniras-MacBook-Air.local>
2019-07-08 09:21:28 +0300

    HW

--- mbta.rb
+++ mbta.rb
@@ -1,4 +1,20 @@
 
+
+# create an hash for the lines
+
+lines = {
+
+"red_line " => ["SS", "PS", "Kendal", "Central", "Harvard", "Porter", "Davis", "Alewife"],
+
+"green_line" =>["GC", "PS", "Boylston", "Arlington", "Copley", "Hynes","Kenmore"],
+
+"orange_line" => ["NS","Haymarket", "PS", "State","DC","Chinatown","BB","FH"] ,
+}
+
+
+def stops_between_stations(start_line, start_station, end_line, end_station)
+end
+
 # your solution here
 def stops_between_stations
 
