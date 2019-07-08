0a053d48726d7bba599053cdf80f1e0f158769ad
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-07-08 07:07:53 +0300

    hw-w04d01

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,40 @@
 
 # your solution here
-def stops_between_stations
 
-end
+
+    def stopsBetweenStations (startLine, startStation, endLine, endStation)
+        subwayLines = {
+    Red: ["South Station","Park Street","Kendall","Central","Harvard","Porter","Davis","Alewife"],
+    Green: ["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"],
+    Orange: ["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hills"] 
+}
+     startStationIndex = subwayLines[startLine].index(startStation)
+     endStationIndex = subwayLines[endLine].index(endStation)
+  
+    if startLine == endLine 
+      (startStationIndex - endStationIndex).abs
+    end 
+
+
+     startLineParkStreetIndex = subwayLines[startLine].index('Park Street')
+  
+
+      
+       tripToParkStreet = (startStationIndex - startLineParkStreetIndex).abs
+  
+      
+       endLineParkStreetIndex = subwayLines[endLine].index('Park Street')
+    
+      
+       tripToDestination = (endStationIndex - endLineParkStreetIndex).abs
+    
+  
+       totalTrip = tripToParkStreet + tripToDestination
+    
+     
+      p totalTrip
+
+    end
+    
+
+stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife')
